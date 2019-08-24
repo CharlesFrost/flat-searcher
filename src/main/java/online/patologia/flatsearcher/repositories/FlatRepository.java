1b64b0cd38ba0b8d37d071b1775981033cc1f8b3
Karol Mróz <karolmroz@protonmail.com>
2019-08-25 01:33:51 +0200

    FIXED QUERY

--- src/main/java/online/patologia/flatsearcher/repositories/FlatRepository.java
+++ src/main/java/online/patologia/flatsearcher/repositories/FlatRepository.java
@@ -1,10 +1,12 @@
 package online.patologia.flatsearcher.repositories;
 
 import online.patologia.flatsearcher.model.Flat;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FlatRepository extends CrudRepository<Flat,Long> {
-    Flat findByLink(String link);
+    @Query(value = "SELECT * FROM FLAT WHERE LINK LIKE=?1", nativeQuery = true)
+    Flat findByLinkCustom(String link);
 }
