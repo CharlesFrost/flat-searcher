b421750bad1133bb21162138b02f81663358bc84
Karol Mróz <karolmroz@protonmail.com>
2019-08-25 10:25:43 +0200

    async mail sending added

--- src/main/java/online/patologia/flatsearcher/service/EmailService.java
+++ src/main/java/online/patologia/flatsearcher/service/EmailService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +14,7 @@ public class EmailService {
 
     @Autowired
     public JavaMailSender emailSender;
-
+    @Async
     public void sendSimpleMessage(
             String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
